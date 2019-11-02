c688486e8b3f6621e6a892398f373e15252ac333
mehdigoom <medhi.gay@sfr.fr>
2019-11-02 22:27:39 +0100

    commit

--- server.js
+++ server.js
@@ -5,25 +5,22 @@ function Server(opt) {
     this.start();
 }
 Server.prototype.start = function() {
+    let room_name = "Room1"
     console.log("Lancement du serveur mulijoueurs Valjang Engine.")
     that = this;
     this.io.on('connection', function(socket) {
         console.log("Le client " + socket.id + " est connecter")
         socket.broadcast.emit('print_server_message', "le client" + socket.id + "est connecter!");
+        socket.to(room_name).emit('print_server_message', "|" + room_name + "|" + "L'utilisateur " + socket.id + "à rejoin la game!")
+        socket.join(room_name)
 
-        socket.on('join_room', function(username, room_name) {
-            console.log(room_name + "L'utilisateur " + username + "à rejoin la game!")
-            socket.to(room_name).emit('print_server_message', "|" + room_name + "|" + "L'utilisateur " + username + "à rejoin la game!")
-            socket.join(room_name)
-
-        })
-        socket.on('leave_room', function(username, room_name) {
+        socket.on('leave_room', function(username) {
             console.log("Lutilisateur :" + username + "à quité le salon" + room_name)
             socket.to(room_name).emit('print_server_message', "|" + room_name + "| l'utilsateur " + "est partie")
             socket.leave(room_name);
         })
 
-        socket.on('my_name_is', function(username, room_name) {
+        socket.on('my_name_is', function(username) {
 
             console.log("le client " + socket.id + "à le nom " + username)
 
@@ -31,12 +28,12 @@ Server.prototype.start = function() {
 
 
         })
-        socket.on('my_posision', function(posisionx, posisiony, room_name) {
+        socket.on('my_posision', function(posisionx, posisiony) {
 
-            console.log("le client :" + socket.id + " est en posision pos X :" + posisionx + " Pos Y" + posisiony)
+            //console.log("le client :" + socket.id + " est en posision pos X :" + posisionx + " Pos Y" + posisiony)
             let id = socket.id
             socket.to(room_name).emit('print_server_message', posisionx, posisiony)
-
+                //console.log(room_name)
 
         })
 
