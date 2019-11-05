88250b3f0a3c0f97c1901e191110e774eb20591c
mehdigoom <medhi.gay@sfr.fr>
2019-11-05 10:19:11 +0100

    spawn item

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ function Server(opt) {
     this.start();
 }
 Server.prototype.start = function() {
+
     console.log("Lancement du serveur mulijoueurs Valjang Engine.")
     that = this;
     this.io.on('connection', function(socket) {
@@ -17,14 +18,22 @@ Server.prototype.start = function() {
 
         socket.on('my_posision', function(posisionx, posisiony, id) {
 
-            console.log("le client :" + id + " est en posision pos X :" + posisionx + " Pos Y" + posisiony)
+            //console.log("le client :" + id + " est en posision pos X :" + posisionx + " Pos Y" + posisiony)
 
             socket.broadcast.emit('player_pos', posisionx, posisiony, id);
             //console.log(room_name)
 
         })
 
+        socket.on('spawnI', function(posisionx, posisiony) {
 
+            console.log("le client :  a spwn  pos X :" + posisionx + " Pos Y" + posisiony)
+
+            socket.broadcast.emit('spawner', posisionx, posisiony);
+            //console.log(room_name)
+
+        })
+        
         socket.on('disconnect', function() {
             console.log("Le client " + socket.id + " est déconnecter")
         });
