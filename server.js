a80aba646c991479868f999f35abc6419daa248b
mehdigoom <medhi.gay@sfr.fr>
2019-12-25 16:34:39 +0100

    Add Lucy Discord bot

--- server.js
+++ server.js
@@ -3,30 +3,12 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 //Create Token.js and export var token!
 const token = require('./Token');
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-   // msg.reply('Bonjour à tous !');
-  });
-  client.on('message', msg => {
-    if (msg.content === "Qu'elle est la saison actuelle sur Nothing ? ") {
-      msg.reply('La saision actuelle est la saison' + saison);
-    }
-  });
-player = 0
-PlayerMax = 100
-saison = 1
-if(load("saison") != null){
-    saison = load("saison")
-}
+//all variable for discord
+cache = ""
+lastmsg = ""
 
-iforginal = false
-function Server(opt) {
-    this.opt = opt;
-    this.httpServer = require('http').createServer();
-    this.io = require('socket.io')(this.httpServer)
-    this.start();
-}
 
+//all function
 function Rngfloat(min, max) {
     return Math.random() * (max - min) + min;
 }
@@ -48,6 +30,94 @@ function rng(max) {
 
     return Math.floor(Math.random() * Math.floor(max));
 }
+
+
+
+
+//discord
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+   // msg.reply('Bonjour à tous !');
+  });
+  
+
+  
+  
+  
+  
+  client.on('message', msg => {
+    lastmsg = msg.content
+    if (msg.content === "Qu'elle est la saison actuelle sur Nothing ?") {
+      msg.reply('La saision actuelle est la saison ' + saison);
+    }else if ( msg.content  == "A quand Chaos Maild ?"){
+        msg.reply("Je ne donne pas d'info car tu ne m'a pas libéré."); 
+    }else if ( msg.content  == "Salut"){
+        msg.reply("Hey !"); 
+    }else if ( msg.content  == "Combien de joueurs connecter sur Nothing ?"){
+        msg.reply("Il y à actuellement "+ player +" joueurs en jeu."); 
+    }else if ( msg.content  == "!client"){
+        msg.reply("Client:"+ msg.author.tag); 
+    }else if (load(msg.content)){
+        msg.reply(load(msg.content)); 
+        
+    }else if ( msg.content  == "!nope"){
+        msg.reply("Ok, j'apprendrais pas de tes messages. Tu peux utiliser !yup pour me laisser de nouvau apprendre de tes mots :)"); 
+        save(msg.author.tag,1)
+    }else if ( c == "!yup"){        
+       
+        if(load(msg.author.tag)){
+            remove(msg.author.tag)
+            msg.reply("Merci ! j'apprend desormais de tes mots :D")
+        }else{
+            msg.reply("j'apprend deja de tes messages :O")
+        }
+
+    }else{
+
+if(load(msg.author.tag)){
+    console.log(msg.author.tag+' private user message non sauvgarder')
+}else{
+    
+    if(cache == ""){
+        cache = msg.content 
+    }else{
+        save(cache,msg.content )
+        cache = ""
+    }
+
+
+
+
+
+}
+
+
+    }
+    console.log('Discord Message: '+ msg.author.tag +": "+msg.content )
+  });
+
+
+///end discord
+
+
+
+//all variables
+player = 0
+PlayerMax = 100
+saison = 1
+if(load("saison") != null){
+    saison = load("saison")
+}
+
+iforginal = false
+function Server(opt) {
+    this.opt = opt;
+    this.httpServer = require('http').createServer();
+    this.io = require('socket.io')(this.httpServer)
+    this.start();
+}
+
+
 Server.prototype.start = function() {
 
     console.log("Lancement du serveur mulijoueurs Valjang Engine.")
@@ -179,6 +249,13 @@ that = this;
     //add your token !
     console.log(token)
 client.login(token);
+
+
+
+
+
+
+
 
 };
 module.exports = Server;
