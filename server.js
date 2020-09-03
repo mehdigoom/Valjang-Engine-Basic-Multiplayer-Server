bb2fd56019d4b97e4bfbbb02ce1c8c2ec49017b0
mehdigoom <medhi.gay@sfr.fr>
2020-09-03 17:03:48 +0200

    Fix voice

--- server.js
+++ server.js
@@ -80,20 +80,24 @@ client.on('ready', () => {
 
 
 
-client.on('message', msg => {
+client.on('message', async message =>  {
 
 
 
-    if (!message.guild) return;
 
-    if (message.content === '/join') {
-      // Only try to join the sender's voice channel if they are in one themselves
-      if (message.member.voice.channel) {
-        const connection = await message.member.voice.channel.join();
-      } else {
-        message.reply("hé ! je peux pas te rejoindre si tu n'est pas dans un salon");
-      }
-    }
+        // Voice only works in guilds, if the message does not come from a guild,
+        // we ignore it
+        if (!message.guild) return;
+      
+        if (message.content === '/join') {
+          // Only try to join the sender's voice channel if they are in one themselves
+          if (message.member.voice.channel) {
+            const connection = await message.member.voice.channel.join();
+          } else {
+            message.reply("Je peux pas te rejoindre si tu n'est pas en vocal :(");
+          }
+        }
+    
 
 
     if (load(msg.author.tag)) {
