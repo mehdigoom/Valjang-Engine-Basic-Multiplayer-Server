d7e6ba24aabd8d7d5b9b8e3e16af7de42ef646fe
mehdigoom <medhi.gay@sfr.fr>
2020-09-03 17:12:43 +0200

    Test

--- server.js
+++ server.js
@@ -80,17 +80,7 @@ client.on('ready', () => {
 
 client.on('message', async message => {
     // Voice only works in guilds, if the message does not come from a guild,
-    // we ignore it
-    if (!message.guild) return;
-  
-    if (message.content === '/join') {
-      // Only try to join the sender's voice channel if they are in one themselves
-      if (message.member.voice.channel) {
-        const connection = await message.member.voice.channel.join();
-      } else {
-        message.reply('You need to join a voice channel first!');
-      }
-    }
+    channel.send('Hi ', { tts: true });
   });
 
 client.on('message', msg => {
